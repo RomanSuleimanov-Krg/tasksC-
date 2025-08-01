5d623296801870f4dbb14fc05e1ea1019ba0c9e7
RomanSuleimanov <sulroman2006@mail.ru>
2025-08-01 13:34:02 +0700

    Решил 1-ю задачку

---
+++ tasksC#/tasksC#/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasksC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЗАДАЧА 1
+            //Введите первое число: 5
+            //Введите второе число: 10
+
+            //Исходные значения:    [a=5]   [b=10]
+            //После модификаторов:  [a=6]   [b=9]
+
+            //+----------------+----------------+
+            //| Операция       | Результат      |
+            //+----------------+----------------+
+            //| Сумма          |             15 | 
+            //| Разность       |             -3 | 
+            //| Произведение   |             50 | до модификаторов
+            //| Среднее        |           7.50 |
+            //| Целое деление  |              0 |
+            //| Остаток        |              6 |
+            //+----------------+----------------+
+
+            int firstNumber, secondNumber;
+
+            Console.Write("Введите первое число: ");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\nИсходные значения:      [a={firstNumber++}]    [b={secondNumber--}]");
+            Console.WriteLine($"После модификаторов:    [a={firstNumber}]    [b={secondNumber}]\n");
+
+            Console.WriteLine($"+----------------+----------------+");
+            Console.WriteLine($"| Операция       | Результат      |");
+            Console.WriteLine($"+----------------+----------------+");
+            Console.WriteLine($"| Сумма          |             {firstNumber + secondNumber} |");
+            Console.WriteLine($"| Разность       |             {firstNumber-- - secondNumber++} |");
+            Console.WriteLine($"| Произведение   |             {firstNumber * secondNumber} |");
+            Console.WriteLine($"| Среднее        |            {(Convert.ToDouble(++firstNumber) + --secondNumber) / 2} |");
+            Console.WriteLine($"| Целое деление  |              {(firstNumber / secondNumber)} |");
+            Console.WriteLine($"| Остаток        |              {firstNumber % secondNumber} |");
+            Console.WriteLine($"+----------------+----------------+");
+
+
+            //AI оценила данное решение на 2/10
+        }
+    }
+}
---
+++ tasksC#/tasksC#/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("tasksC#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tasksC#")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("96880f0a-7d86-4fb5-88c2-8a8aa41d90dc")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
