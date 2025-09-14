fbe265a76851a71bd6f009d53b38c81219cb4985
Mariya Krechmer <sulroman2006@mail.ru>
2025-09-14 22:35:43 +0700

    Начал делать виртуальную библиотеку. Создал класс Book с нужными для библиотеки полями. Сделал интерфейс который реализует класс InMemoryBooksRepository. Выполнил часть контракта интерфейса IBookRepository. Делал попытки использования LINQ вместо перебора и поиска через foreach.

---
+++ task3C#/task3C#/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface IBookRepository
+    {
+        void AddBook(Book book);
+        Book GetBookId(int id);
+        List<Book> GetAllBooks();
+        List<Book> GetBooksByAuthor(string author);
+        bool UpdateBook(Book book);
+        bool DeleteBook(int id);
+    }
+    class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        
+        public Book(int id, string title, string author, int year)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Id},{Title},{Author},{Year}");
+        }
+    }
+
+    class InMemomoryBookRepository : IBookRepository
+    {
+        private List<Book> _books = new List<Book>();
+
+        public void AddBook( Book book )
+        {
+            _books.Add( book );
+        }
+
+        //Я узнал что для нахождения книги по id, при помощи LINQ я могу использовать
+        //методы FirstOrDefault() или SingleOrDefault(). Я выбрал второе т.к. в
+        //библиотеке каждая книга имеет уникальный id. Немного изучив LINQ я пришел
+        //к мнению, что будет проще использовать его чем я бы искал нужный id книги
+        //через foreach
+        public Book GetBookId(int id)
+        {
+            var resultFinfBook = _books.SingleOrDefault(book => book.Id == id);
+            return resultFinfBook;
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            //var allBooks = _books.Select(book => new Book { Id = book.Id, Title = book.Title, Author = book.Author, Year = book.Year }).ToList;
+            return _books;
+        }
+    }
+}
---
+++ task3C#/task3C#/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("task3C#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task3C#")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f7e7fc29-090f-4aa9-b61a-9fbb960677f6")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
